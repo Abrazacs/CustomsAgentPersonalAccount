f2f3db7c677915384f02e65d4733fcc91c0c24fb
Алексей <avlapshov@dasreda.ru>
2022-11-19 22:51:46 +0300

    рефакторинг

--- personal-account-service/src/main/java/ru/custom/agent/personal/account/personal/Application.java
+++ personal-account-service/src/main/java/ru/custom/agent/personal/account/personal/Application.java
@@ -1,4 +1,4 @@
-package ru.custom.agent.personal.account.auth;
+package ru.custom.agent.personal.account.personal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
