e4326eadf4cd763e396cc62e53a0dc5c959f8041
Ilya <ilyha575@inbox.ru>
2022-12-11 20:30:57 +1000

    Added admin filter for auth + fix
    Добавлен фильтр доступа к auth сервису, мелкие исправления

--- auth-service/src/main/java/ru/ssemenov/exceptions/AppError.java
+++ auth-service/src/main/java/ru/ssemenov/exceptions/AppError.java
@@ -1,30 +1,23 @@
 package ru.ssemenov.exceptions;
 
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 import ru.ssemenov.dtos.Violation;
 
 import java.util.List;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
-@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class AppError {
-    public AppError(String code, String message) {
-        new AppError(code, message, null);
-    }
-
     @Schema(description = "Код ошибки", required = true, example = "RESOURCE_NOT_FOUND")
-    private String code;
+    private final String code;
 
     @Schema(description = "Текст ошибки", required = true,
             example = "This login is occupied. Try to use another one")
-    private String message;
+    private final String message;
 
     @Schema(description = "Список нарушений", required = true)
     private List<Violation> violations;
--- core-service/src/main/java/ru/ssemenov/exceptions/AppError.java
+++ core-service/src/main/java/ru/ssemenov/exceptions/AppError.java
@@ -2,23 +2,21 @@ package ru.ssemenov.exceptions;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import ru.ssemenov.dtos.Violation;
 
 import java.util.List;
 
-@Data
+@Getter
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class AppError {
-    public AppError(String code, String message) {
-        new AppError(code, message, null);
-    }
-
     @Schema(description = "Код ошибки", required = true, example = "RESOURCE_NOT_FOUND")
-    private String code;
+    private final String code;
 
     @Schema(description = "Текст ошибки", required = true, example = "Декларация с id=684e4ea4-cac9-4b33-843e-d26274ff9f7e не существует!")
-    private String message;
+    private final String message;
 
     @Schema(description = "Список нарушений", required = true)
     private List<Violation> violations;
