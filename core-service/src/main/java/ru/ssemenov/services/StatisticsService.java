e911c9e571f6175dcffd8496cf68087ccd4479d1
Aleksandr <Berezhnoy_aa@mail.ru>
2022-12-12 17:21:20 +0300

    backend for statistic updated

--- core-service/src/main/java/ru/ssemenov/services/StatisticsService.java
+++ core-service/src/main/java/ru/ssemenov/services/StatisticsService.java
@@ -2,17 +2,9 @@ package ru.ssemenov.services;
 
 import org.springframework.stereotype.Service;
 import ru.ssemenov.dtos.StatisticsDto;
-import ru.ssemenov.entities.CustomsDeclaration;
-import ru.ssemenov.exceptions.ResourceNotFoundException;
-import ru.ssemenov.utils.CustomsDeclarationStatusEnum;
-
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.List;
 
 @Service
 public interface StatisticsService {
 
-    public StatisticsDto getStatistics();
+    StatisticsDto getStatistics();
 }
