92ca7dfe23368818e2285d4eb54735835776f3a8
Aleksandr <Berezhnoy_aa@mail.ru>
2022-12-12 17:06:37 +0300

    backend for statistic updated

--- core-service/src/main/java/ru/ssemenov/controllers/StatisticsController.java
+++ core-service/src/main/java/ru/ssemenov/controllers/StatisticsController.java
@@ -1,15 +1,10 @@
 package ru.ssemenov.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import ru.ssemenov.dtos.StatisticsDto;
 
 @Controller
 public interface StatisticsController {
 
-    @Override
-    @GetMapping("/statistics")
-    public StatisticsDto getStatistics() {
-        return statisticsService.getStatistics();
-    }
+    StatisticsDto getStatistics();
 }
--- core-service/src/main/java/ru/ssemenov/controllers/impl/StatisticsControllerImpl.java
+++ core-service/src/main/java/ru/ssemenov/controllers/impl/StatisticsControllerImpl.java
@@ -1,11 +1,21 @@
 package ru.ssemenov.controllers.impl;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.ssemenov.controllers.StatisticsController;
 import ru.ssemenov.dtos.StatisticsDto;
+import ru.ssemenov.services.StatisticsService;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/declarations/statistics")
 public class StatisticsControllerImpl implements StatisticsController {
+
+    private final StatisticsService statisticsService;
+
     @Override
     public StatisticsDto getStatistics() {
-        return null;
+        return statisticsService.getStatistics();
     }
 }
