efc2615c9923bf84b3ba354cf57e3b0fcb2742bd
Aleksandr <Berezhnoy_aa@mail.ru>
2022-12-25 13:34:32 +0300

    clean core and auth DB's

--- auth-service/src/main/resources/db/changelog/db.changelog-master.sql
+++ auth-service/src/main/resources/db/changelog/db.changelog-master.sql
@@ -2,44 +2,47 @@
 
 -- changeset author: sergey
 
-create table roles (
-    id          UUID primary key,
-    name        varchar(255) unique not null,
+create table roles
+(
+    id UUID primary key,
+    name       varchar(255) unique not null,
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP default current_timestamp
+);
+
+create table users
+(
+    id UUID primary key,
+    username    varchar(255) not null unique,
+    password    varchar(255) not null,
+    email       varchar(255) not null,
+    company_vat varchar(255) not null,
     created_at  TIMESTAMP default current_timestamp,
     updated_at  TIMESTAMP default current_timestamp
 );
 
-create table users(
-    id              UUID primary key,
-    username        varchar(255) not null unique,
-    password        varchar(255) not null,
-    email           varchar(255) not null,
-    company_vat     varchar(255) not null,
-    created_at      TIMESTAMP default current_timestamp,
-    updated_at      TIMESTAMP default current_timestamp
-);
-
-create table users_roles (
-    user_id     UUID not null references users (id),
-    role_id     UUID not null references roles (id),
-    created_at  TIMESTAMP default current_timestamp,
-    updated_at  TIMESTAMP default current_timestamp,
+create table users_roles
+(
+    user_id UUID not null references users (id),
+    role_id UUID not null references roles (id),
+    created_at TIMESTAMP default current_timestamp,
+    updated_at TIMESTAMP default current_timestamp,
     primary key (user_id, role_id)
 );
 
 insert into users (id, username, password, email, company_vat)
-values ('ec2aa7f4-42d5-442c-8baf-6d4cc6d15db3','admin', '$2a$12$i2wJXQxdTdUMK7Zg5TSrX.LLuqI0SMKeiNmP3xdF3UEeaSipB6Nqa', 'admin@admin.com', '7777777777'),
-       ('fa6f0504-54bd-42b1-8363-7970492e724d','abrazacs', '$2a$12$NfP7TBABXE1TzpoTfEjlAOoFGmPAJe6HVyGc3DSjuuaUWcb6WOyC2','sergeysemenov87@gmail.com','6767676767'),
-       ('9343e308-0dda-4b1a-bf8b-64819eee192e','nonepostlife', '$2a$12$NfP7TBABXE1TzpoTfEjlAOoFGmPAJe6HVyGc3DSjuuaUWcb6WOyC2', 'mail@mail.com', '1234567891'),
-       ('45a59d12-f2da-4415-a0d0-d15eac113560','aberezhnoy', '$2a$12$YD5Q1aV.UcGzoCxDv0PLBOHk6ZZKfVyC2zjJ35x.7UA4PWb1uoESu', 'berezhnoy_aa@mail.ru', '2345676543');
+values ('ec2aa7f4-42d5-442c-8baf-6d4cc6d15db3', 'admin', '$2a$12$i2wJXQxdTdUMK7Zg5TSrX.LLuqI0SMKeiNmP3xdF3UEeaSipB6Nqa',
+        'admin@admin.com', '7777777777'),
+       ('fa6f0504-54bd-42b1-8363-7970492e724d', 'abrazacs',
+        '$2a$12$NfP7TBABXE1TzpoTfEjlAOoFGmPAJe6HVyGc3DSjuuaUWcb6WOyC2', 'sergeysemenov87@gmail.com', '6767676767'),
+       ('9343e308-0dda-4b1a-bf8b-64819eee192e', 'nonepostlife',
+        '$2a$12$NfP7TBABXE1TzpoTfEjlAOoFGmPAJe6HVyGc3DSjuuaUWcb6WOyC2', 'mail@mail.com', '1234567891');
 
 insert into roles (id, name)
-values('f3db87e9-738e-46c8-990f-30206f38131a', 'ROLE_USER'),
-      ('0b103c7e-9110-43cf-8236-4db3f12095c6', 'ROLE_ADMIN');
+values ('f3db87e9-738e-46c8-990f-30206f38131a', 'ROLE_USER'),
+       ('0b103c7e-9110-43cf-8236-4db3f12095c6', 'ROLE_ADMIN');
 
 insert into users_roles (user_id, role_id)
 values ('ec2aa7f4-42d5-442c-8baf-6d4cc6d15db3', 'f3db87e9-738e-46c8-990f-30206f38131a'),
        ('ec2aa7f4-42d5-442c-8baf-6d4cc6d15db3', '0b103c7e-9110-43cf-8236-4db3f12095c6'),
-       ('fa6f0504-54bd-42b1-8363-7970492e724d', 'f3db87e9-738e-46c8-990f-30206f38131a'),
-       ('9343e308-0dda-4b1a-bf8b-64819eee192e', 'f3db87e9-738e-46c8-990f-30206f38131a'),
-       ('45a59d12-f2da-4415-a0d0-d15eac113560', 'f3db87e9-738e-46c8-990f-30206f38131a');
+       ('fa6f0504-54bd-42b1-8363-7970492e724d', 'f3db87e9-738e-46c8-990f-30206f38131a');
