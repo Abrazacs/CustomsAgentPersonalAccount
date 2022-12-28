18d586feb277a18112d5046e03a32ac931b6f271
Abrazacs <84628800+Abrazacs@users.noreply.github.com>
2022-12-28 04:45:09 +0300

    RabbitMQ для пересылки сообщений о изменении статуса ДТ в сервис нотификаций

--- notification-service/src/main/java/ru/ssemenov/utils/RabbitConsumer.java
+++ notification-service/src/main/java/ru/ssemenov/utils/RabbitConsumer.java
@@ -1,13 +1,18 @@
 package ru.ssemenov.utils;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import ru.ssemenov.exceptions.MailException;
 import ru.ssemenov.services.impl.MailServiceImpl;
+
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class RabbitConsumer {
 
     private static final String TITLE = "Notification";
@@ -15,10 +20,16 @@ public class RabbitConsumer {
 
     @RabbitListener(queues = "Notifications")
     public void receiveMessage(String message){
+        UUID trace = UUID.randomUUID();
         String[] strings = message.split(" ");
         String vatCode = strings[0];
         String msg = String.format("Статус декларации № %s изменился на %s", strings[1], strings[2]);
-        mailService.notifyMailRecipients(vatCode, TITLE, msg);
+        try {
+            mailService.notifyMailRecipients(vatCode, TITLE, msg);
+        }catch (Exception e){
+            log.info("Error occurred while trying to send the message, trace={} ", trace);
+        }
+        System.out.println(msg);
     }
 
 }
