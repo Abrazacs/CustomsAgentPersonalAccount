051fb1e888e8b656ff8c8dd7e52a06cef29a60ac
Ilya <ilyha575@inbox.ru>
2022-12-28 11:50:22 +1000

    ++

--- core-service/src/main/java/ru/ssemenov/utils/NotificationProducer.java
+++ core-service/src/main/java/ru/ssemenov/utils/NotificationProducer.java
@@ -1,17 +1,11 @@
 package ru.ssemenov.utils;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.UUID;
-import java.util.concurrent.TimeoutException;
 
 @Slf4j
 @Component
@@ -19,22 +13,12 @@ import java.util.concurrent.TimeoutException;
 public class NotificationProducer {
 
     private static final String NOTIFICATION_QUEUE = "notifications";
-    //    private final ConnectionFactory connectionFactory;
     private final RabbitTemplate rabbitTemplate;
 
     public void publishNotification(String message) {
         UUID trace = UUID.randomUUID();
         log.info("Start send message to notification, traceId={}", trace);
         rabbitTemplate.convertAndSend(NOTIFICATION_QUEUE, message);
-
-//        try (Connection connection = connectionFactory.newConnection();
-//             Channel channel = connection.createChannel()) {
-//            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-//            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
-//            log.info("Message successfully published, trace={}", trace);
-//        } catch (IOException | TimeoutException e) {
-//            log.error("Error while trying to establish connection, error={}, traceId={}", e.getMessage(), trace);
-//        }
-
+        log.info("Message successfully published, trace={}", trace);
     }
 }
